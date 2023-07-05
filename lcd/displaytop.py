e650de70154b7307b3cb25d175c7e1edc5c2af24
hummusbird <birb@birb.cc>
2023-07-05 22:21:17 +0100

    remove prints

--- lcd/displaytop.py
+++ lcd/displaytop.py
@@ -6,7 +6,5 @@ lcd = I2C_LCD_driver.lcd()
 input = sys.stdin.read().rstrip()
 
 padlen = (16 - len(input)) / 2
-print(padlen)
 paddedstr = " " * padlen + input + " " * padlen
-print(paddedstr)
 lcd.lcd_display_string(paddedstr, 1)
--- player/floppy.sh
+++ player/floppy.sh
@@ -2,7 +2,7 @@
 
 path="/mnt/floppy/"
 song="song.opus"
-drive="sdb"
+drive="sda"
 chkdrive="lsblk --output NAME --include 8 --noheadings /dev/${drive}"
 
 while true
@@ -10,27 +10,25 @@ do
   disk="$($chkdrive)"
 
   if [[ $disk != $drive ]]; then
-    echo "no disk inserted"
-    echo "no disk!" | python ../lcd/displaytop.py 
+    echo "no disk!" | python ../lcd/displaytop.py & 
     sleep 2 
 
   elif [[ -f $path$song ]]; then
-    echo "playing $path$song"
-    echo "playing $path$song" | python ../lcd/scroll.py
+    echo "playing $path$song" | python ../lcd/scroll.py &
     ffplay "$path$song" -hide_banner -loglevel error -autoexit -nodisp &
 
     while [[ "$($chkdrive)" == $drive ]]
     do
       sleep 0.2
-      echo "playing..."
     done
 
-    echo "disk removed!" | python ../lcd/displaybottom.py
+    killall python
+    echo "disk removed!" | python ../lcd/displaybottom.py &
     pkill ffplay
     sleep 2
 
   else
-    echo "no song found" | python ../lcd/displaytop.py
+    echo "no song found" | python ../lcd/displaytop.py &
     sleep 2
   fi
 done
