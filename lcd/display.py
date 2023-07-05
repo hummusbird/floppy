a950810f15d60b02ccb6ad05ec5fd1571d4a46b7
hummusbird <birb@birb.cc>
2023-07-05 22:56:09 +0100

    join top and bottom display scripts

---
+++ lcd/display.py
@@ -0,0 +1,18 @@
+import I2C_LCD_driver
+import sys
+
+lcd = I2C_LCD_driver.lcd()
+
+input = sys.stdin.read().split(",")
+input.append("")
+top = input[0].rstrip()
+bot = input[1].rstrip()
+
+toppadlen = (16 - len(top)) / 2
+toppadstr = " " * toppadlen + top + " " * toppadlen
+
+botpadlen = (16 - len(bot)) / 2
+botpadstr = " " * botpadlen + bot + " " * botpadlen
+
+lcd.lcd_display_string(toppadstr, 1)
+lcd.lcd_display_string(botpadstr, 2)
--- player/floppy.sh
+++ player/floppy.sh
@@ -5,12 +5,16 @@ song="song.opus"
 drive="sda"
 chkdrive="lsblk --output NAME --include 8 --noheadings /dev/${drive}"
 
+echo "Floppy Disk Jukebox - V1.0 by @hummusbird"
+echo "FDD Jukebox,V1.0 @hummusbird" | python ../lcd/display.py &
+sleep 2
+
 while true
 do
   disk="$($chkdrive)"
 
   if [[ $disk != $drive ]]; then
-    echo "no disk!" | python ../lcd/displaytop.py & 
+    echo "no disk!" | python ../lcd/display.py & 
     sleep 2 
 
   elif [[ -f $path$song ]]; then
@@ -23,12 +27,12 @@ do
     done
 
     killall python
-    echo "disk removed!" | python ../lcd/displaybottom.py &
+    echo "disk removed!" | python ../lcd/display.py &
     pkill ffplay
     sleep 2
 
   else
-    echo "no song found" | python ../lcd/displaytop.py &
+    echo "no song found" | python ../lcd/display.py &
     sleep 2
   fi
 done
