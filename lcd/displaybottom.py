a1998cffb7f89bd7a6031313a41b6fc206d6c9f7
hummusbird <birb@birb.cc>
2023-07-05 21:37:44 +0100

    centered display

---
+++ lcd/displaybottom.py
@@ -0,0 +1,12 @@
+import I2C_LCD_driver
+import sys
+
+lcd = I2C_LCD_driver.lcd()
+
+input = sys.stdin.read().rstrip()
+
+padlen = (16 - len(input)) / 2
+print(padlen)
+paddedstr = " " * padlen + input + " " * padlen
+print(paddedstr)
+lcd.lcd_display_string(paddedstr, 2)
